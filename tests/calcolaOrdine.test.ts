9a746f82bca517daba8d78d91d3e2be299410e37
luis.huaman <luisitohblas@gmail.com>
2025-04-10 10:35:09 +0200

    Articoli con IVA 22%: FALLITO

--- tests/calcolaOrdine.test.ts
+++ tests/calcolaOrdine.test.ts
@@ -7,4 +7,23 @@ test("1. Array vuoto", () => {
       iva: 0,
       totale: 0,
     });
+
+  });
+
+  test("2. articoli con IVA 22%", () => {
+    const articoli = [
+      {
+        nome: "PC",
+        imponibile: 1000.0,
+        iva: 22,
+      },
+    ];
+  
+    const risultato = calcolaOrdine(articoli);
+  
+    expect(risultato).toEqual({
+      imponibile: 1000.0,
+      iva: 220.0,
+      totale: 1220.0,
+    });
   });
