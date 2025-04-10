cc5ce647558d7b0373219a7e0f1f63f29a3a0102
luis.huaman <luisitohblas@gmail.com>
2025-04-10 12:09:25 +0200

    Calcolo prodotto+spese di spedizione: FUNZIONA

--- src/calcolaOrdineConSpedizione.ts
+++ src/calcolaOrdineConSpedizione.ts
@@ -1,5 +1,23 @@
 import { Articolo } from "./types/type";
 
-export const calcolaOrdineConSpedizione = (articoli:Articolo) => {
-    return{}
+export const calcolaOrdineConSpedizione = (articoli:Articolo[], speseSpedizione: number) => {
+    let totaleImponibile = 0;
+  let totaleIVA = 0;
+
+  for (const articolo of articoli) {
+    totaleImponibile += articolo.imponibile;
+    if (articolo.iva > 0) {
+      totaleIVA += articolo.imponibile * (articolo.iva / 100);
+    }
+  }
+
+  const totaleParziale = totaleImponibile + totaleIVA;
+  const totale = totaleParziale + speseSpedizione;
+
+  return {
+    imponibile: parseFloat(totaleImponibile.toFixed(2)),
+    iva: parseFloat(totaleIVA.toFixed(2)),
+    speseSpedizione: parseFloat(speseSpedizione.toFixed(2)),
+    totale: parseFloat(totale.toFixed(2)),
+  };
 }
