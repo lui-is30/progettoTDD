e6146b6d3b3ade9c67b093ff4ca6f697312bdcd7
luis.huaman <luisitohblas@gmail.com>
2025-04-10 10:25:07 +0200

    Array vuoto: FUNZIONA

--- src/calcolaOrdine.ts
+++ src/calcolaOrdine.ts
@@ -1,5 +1,12 @@
 import { Articolo } from "./types/type";
 
 export const calcolaOrdine = (articoli: Articolo[]) => {
+    if (articoli.length === 0) {
+        return {
+          imponibile: 0,
+          iva: 0,
+          totale: 0,
+        };
+      }
   return {};
 }
